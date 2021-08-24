ea3822a36372bb0ae84078f1650c73356c223eef
kimjuneseo <june3929213512@gmail.com>
2021-08-24 23:10:35 +0900

    08-24

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header.jsx'
+import Prototypes from './components/Prototypes.jsx'
+import Orders from './components/Orders.jsx'
+import Footer from './components/Footer.jsx'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Header/>
+    <div className="container">
+      <Prototypes/>
+      <Orders/>
+      <Footer/>
     </div>
+    </>
   );
 }
 
